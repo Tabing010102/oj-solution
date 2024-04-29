5ed3909806160a47d8dfae44f653365b8e31dac5
Tabing010102 <tabing010102@outlook.com>
2024-04-29 22:51:06 +0800

    add LeetCode 42

---
+++ LeetCode/面试经典 150 题/数组_字符串/42. 接雨水/42.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    inline int min(const int &a, const int &b) { return a < b ? a : b; }
+    inline int max(const int &a, const int &b) { return a > b ? a : b; }
+    int trap(vector<int>& height) {
+        deque<int> dq;
+        int ans = 0;
+        for (int i = 0; i < height.size(); i++) {
+            int cnt = height[i];
+            if (dq.empty() || height[dq.back()] > cnt) { dq.push_back(i); }
+            else if (height[dq.back()] == cnt) {
+                dq.pop_back();
+                dq.push_back(i);
+            } else {
+                while (!dq.empty() && height[dq.back()] < cnt) {
+                    int r = dq.back();
+                    dq.pop_back();
+                    if (!dq.empty()) {
+                        int l = dq.back() + 1;
+                        ans += (min(cnt, height[dq.front()]) - height[r]) * (r - l + 1);
+                    }
+                }
+                while (!dq.empty() && height[dq.back()] == cnt) { dq.pop_back(); }
+                dq.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+//    vector<int> x = {0,1,0,2,1,0,1,3,2,1,2,1};
+    vector<int> x = {4,2,0,3,2,5};
+    Solution s;
+    auto ans = s.trap(x);
+    printf("%d ", ans);
+    return 0;
+}
