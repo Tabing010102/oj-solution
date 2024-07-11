b56663ce10a731124abf9550762e8977d9f41397
Tabing010102 <tabing010102@outlook.com>
2024-07-11 16:52:33 +0800

    add LeetCode 120

---
+++ LeetCode/面试经典 150 题/多维动态规划/120. 三角形最小路径和/120.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+class Solution {
+    public final int MAXN = 200 + 10;
+    public final int INF = Integer.MAX_VALUE;
+    List<List<Integer>> d;
+    int[][] f;
+    int dp(int i, int j) {
+        if (i < j || j < 0) {
+            return 0x3f3f3f3f;
+        }
+        if (i == 0) {
+            return f[i][j] = d.get(i).get(j);
+        }
+        if (f[i][j] != 0) {
+            return f[i][j];
+        }
+        return f[i][j] = d.get(i).get(j) + Math.min(dp(i - 1, j), dp(i - 1, j - 1));
+    }
+    public int minimumTotal(List<List<Integer>> triangle) {
+        d = triangle;
+        f = new int[MAXN][MAXN];
+        for (int i = 0; i < MAXN; i++) {
+            Arrays.fill(f[i], 0);
+        }
+        for (int i = 0; i < d.get(d.size() - 1).size(); i++) {
+            dp(d.size() - 1, i);
+        }
+        int minVal = INF;
+        for (int i = 0; i < d.get(d.size() - 1).size(); i++) {
+            minVal = Math.min(f[d.size() - 1][i], minVal);
+        }
+        return minVal;
+    }
+}
