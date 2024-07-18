732761aa5b1986624afa91ba509a33d50c45a5a0
Tabing010102 <tabing010102@outlook.com>
2024-07-18 10:30:42 +0800

    update LeetCode 72

--- LeetCode/面试经典 150 题/多维动态规划/72. 编辑距离/72-memOn.java
+++ LeetCode/面试经典 150 题/多维动态规划/72. 编辑距离/72-memOn.java
@@ -4,35 +4,42 @@ class Solution {
     // f[i][j] -> w1[i-1] -> w2[j-1]
     public int minDistance(String word1, String word2) {
         String w1 = word1, w2 = word2;
-        int[][] f = new int[w1.length() + 1][w2.length() + 1];
+        int[][] f = new int[w1.length() + 1][2];
         for (int i = 0; i <= w1.length(); i++) {
             Arrays.fill(f[i], 2000);
         }
         f[0][0] = 0;
         for (int j = 0; j <= w2.length(); j++) {
+            int j1 = j % 2;
+            int j0 = (j + 1) % 2;
             for (int i = 0; i <= w1.length(); i++) {
                 // delete
                 if (i > 0) {
-                    f[i][j] = Math.min(f[i][j], f[i - 1][j] + 1);
+                    f[i][j1] = Math.min(f[i][j1], f[i - 1][j1] + 1);
                 }
                 // none
                 if (i > 0 && j > 0 && w1.charAt(i - 1) == w2.charAt(j - 1)) {
-                    f[i][j] = Math.min(f[i][j], f[i - 1][j - 1]);
+                    f[i][j1] = Math.min(f[i][j1], f[i - 1][j0]);
                 } else {
                     // change
                     if (i > 0 && j > 0) {
-                        f[i][j] = Math.min(f[i][j], f[i - 1][j - 1] + 1);
+                        f[i][j1] = Math.min(f[i][j1], f[i - 1][j0] + 1);
                     }
                     // add
                     if (j > 0) {
-                        f[i][j] = Math.min(f[i][j], f[i][j - 1] + 1);
+                        f[i][j1] = Math.min(f[i][j1], f[i][j0] + 1);
                     }
                 }
             }
             // for (int i = 0; i <= w1.length(); i++) {
-            //     System.out.print(Integer.valueOf(f[i][j]).toString() + " ");
+            //     System.out.print(Integer.valueOf(f[i][j1]).toString() + " ");
             // }
             // System.out.println();
+            if (j < w2.length()) {
+                for (int i = 0; i <= w1.length(); i++) {
+                    f[i][j0] = 2000;
+                }
+            }
         }
         // for (int i = 0; i <= w1.length(); i++) {
         //     System.out.println();
@@ -40,6 +47,6 @@ class Solution {
         //         System.out.print(Integer.valueOf(f[i][j]).toString() + " ");
         //     }
         // }
-        return f[w1.length()][w2.length()];
+        return f[w1.length()][w2.length() % 2];
     }
 }
