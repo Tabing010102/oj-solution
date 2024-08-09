b0beb3431cce8a42af417a844e7beb722aa0bfc5
Tabing010102 <tabing010102@outlook.com>
2024-08-09 18:36:11 +0800

    add LeetCode 213

---
+++ LeetCode/Other/213. 打家劫舍 II/213.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 1) { return nums[0]; }
+        int[] f = new int[nums.length];
+        int[] f2 = new int[nums.length];
+        int[] d = nums;
+        f[0] = d[0];
+        f[1] = Math.max(f[0], d[1]);
+        for (int i = 2; i < d.length; i++) {
+            f[i] = Math.max(f[i - 2] + d[i], f[i - 1]);
+        }
+        f2[0] = 0;
+        f2[1] = d[1];
+        for (int i = 2; i < d.length; i++) {
+            f2[i] = Math.max(f2[i - 2] + d[i], f2[i - 1]);
+        }
+        return Math.max(f[d.length - 2], f2[d.length - 1]);
+    }
+}
